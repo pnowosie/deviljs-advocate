b573a7c0e37636d65c221ecab89409d84ba63905
Pawel Nowosielski <nowosielski@gmail.com>
2013-12-23 18:51:01 +0100

    Added Mocha for test and Chai for assertions

---
+++ JsModularity/tests/00-namespaces.js
@@ -0,0 +1,28 @@
+﻿describe('Namespaces', function() {
+    it('Should create empty objects', function () {
+        var _app = window.App;
+        expect(_app).to.not.exist;
+
+        namespace('App.Module1');
+
+        App.should.exists;
+        App.Module1.should.exists;
+    });
+    
+    it('Should extend existing objects', function () {
+        namespace('App.Module1');
+        namespace('App.Module2');
+        
+        App.should.exists;
+        App.Module1.should.exists;
+        App.Module2.should.exists;
+
+        App.Module2.value = 1;
+
+        namespace('App.Module2.Submodule1');
+        App.Module1.should.exists;
+        App.Module2.should.exists;
+        App.Module2.Submodule1.should.exists;
+        App.Module2.value.should.be.equal(1);
+    });
+});
---
+++ JsModularity/util/namespace.js
@@ -0,0 +1,19 @@
+﻿var namespace = (function() {
+
+    var global = window || {},
+        namespace;
+
+    namespace = function (nsString) {
+        var module = global,
+            parts = nsString.split('.');
+
+        for (var i = 0, len = parts.length; i < len; ++i) {
+            var part = parts[i];
+
+            module[part] = module[part] || {};
+            module = module[part];
+        }
+    };
+
+    return namespace;
+})();
