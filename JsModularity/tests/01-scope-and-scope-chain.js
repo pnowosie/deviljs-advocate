a1bde7a72d073c988243fa0e6f97501e86497ace
Pawel Nowosielski <nowosielski@gmail.com>
2014-01-15 09:52:30 +0100

    Functions

--- JsModularity/tests/01-scope-and-scope-chain.js
+++ JsModularity/tests/01-scope-and-scope-chain.js
@@ -1,5 +1,59 @@
 ﻿"use strict";
+/*
+    - Scope w JS tworzy tylko funkcja
 
+ */
 describe('01. Scope and scope chain', function () {
-    it('Samples');
+    
+    it('Function scope', function() {
+        should.exist(newScope, 'function newScope()');
+        newScope.should.be.a('function');
+        should.not.exist(a);
+
+
+        var a = 1, b;
+
+        a.should.be.equal(1);
+        should.not.exist(b);
+        should.not.exist(c);
+        //should.not.exist(d);       <- d is not defined
+
+        newScope.should.be.a('function');
+
+        b = 2;
+        a.should.be.equal(1);
+        b.should.be.equal(2);
+
+
+        //var newScope =
+        function newScope(x) {
+            
+
+            x.should.be.equal(9);   //<======== 
+                                              //
+            
+            
+            a.should.be.equal(1);          //
+            b.should.be.equal(2);         //
+            should.not.exist(c);         //
+            var c = 5;
+
+            var inF = 8;              //
+        }                            //
+        newScope(9);          // <====
+
+        should.not.exist(c);
+        var c = 0;
+        a.should.be.equal(1);
+    });
+
+    it('IFI - it is good to know this js pattern', function() {
+
+        // IFI == Immediate function invocation
+        (function() {           // <-- annonymous function
+
+            /* some code */
+        })();                   // <-- immediate invocation
+
+    });
 });
--- JsModularity/tests/03-function-and-closure.js
+++ JsModularity/tests/03-function-and-closure.js
@@ -5,9 +5,124 @@
     Funkcja czyli instancja obiektu, ile funkcji tu utworzyliśmy?
     Funkcja-konstruktor i jej właściwości
  */
+var x = 'global scope';
+var obj = {
+    x: 'obj',
+    f: function() { return this.x; }  
+};
+var f = obj.f;      // <-- window.f = obj.f;
+    
+
+describe('03.0. Function and ways of invocation', function() {
+    // UWAGA: 'this' - nie ma nic (za dużo) wspólnego z obiektem, w którym zadeklarowaliśmy funkcję
+    // Dowód:
 
 
-describe('03. Function and closure', function() {
+    it('Invocation as a method', function() {
+        obj.f().should.be.equal('obj');
+    });
 
-    it('what is closure?');
+    it('Normal function invocation', function() {
+        //throw 'dupa';
+        f.should.be.a('function');
+        (window.f()).should.be.equal('global scope');
+    });
+
+    it('Apply/Call invocation', function() {
+        // spreparujemy obiekt na którym wywołamy funkcje
+        var myObj = { x: 'myObj rulez!' };
+
+        var result = obj.f.call(myObj /*, params*/);
+        // każda funkcja ma ^--      -- 'metodę' call
+        // pierwszym parametrem  ^- jest obiekt 'this'
+    });
+
+    it('Bind - we\'re not defenseless [out scope - sorry]');
+});
+
+describe('03.1. Function and closure', function() {
+
+    it('what is closure?', function() {
+
+        var f = function(p) {
+
+            var a = 1;
+
+            var pf = function () {   // this is private
+                var b = 3;
+                return a * p + b;
+            };
+
+            return function() { return pf(); };
+        };
+
+        var g = function(fun) {
+            // pf: a * p + b
+            // a == 1, b == 3;
+            // p == 2 (below)
+            // pf() == 1 * 2 + 3 = 5
+            (5).should.be.equal( fun() ) ;
+        };
+
+        g(f(2));
+    });
+
+    it('Closure - I don\'t get it', function() {
+
+        // Tablica funArr[0..9] z funkcjami 
+        // chcemy by: f_i = funArr[i], f_i() == i (zwracała swój index w tablicy) 
+
+        var funArr = new Array(10), len = funArr.length;
+        for (var i = 0; i < len; i++)
+            funArr[i] = function () { return i; };
+
+        funArr.should.have.length(len);
+        (10).should.be.equal( funArr[0]() );      // Oops!
+        (10).should.be.equal( funArr[9]() );      // as above ;)
+
+    });
+
+    it('How many functions I\'ve created?', function() {
+
+        var funArr = new Array(10), len = funArr.length;
+        for (var i = 0; i < len; i++)
+            funArr[i] = (function(j) {
+                 return function() { return j; };
+            })(i);
+        
+        funArr.should.have.length(len);
+        (0).should.be.equal( funArr[0]() );      // Tak ma być!
+        (9).should.be.equal( funArr[9]() );      // ...tylko tych funkcji za dużo :(
+    });
+
+    it('Closure - it\'s (should be) simple now', function() {
+
+        var funArr = new Array(10), len = funArr.length;
+        var funFactory = function(j) {
+            return function () { return j; };
+        };
+
+        for (var i = 0; i < len; i++)
+            funArr[i] = funFactory(i);
+        
+        funArr.should.have.length(len);
+        (0).should.be.equal( funArr[0]() );      // Tak ma być!
+        (9).should.be.equal( funArr[9]() );      // The (happy) end
+    });
+
+});
+
+describe('03.2. Constructor functions', function() {
+
+    it('new - operator with function call', function() {
+
+        var Cat = function(owner) {      // <-- Wielką literą (konwencja)
+            this.owner = owner;
+            this.say = function() { return 'Miał!'; };
+        };
+
+        var mruczek = new Cat('Ala');
+        mruczek.say().should.be.equal('Miał!');
+        mruczek.owner.should.be.equal('Ala');
+    });
 });
